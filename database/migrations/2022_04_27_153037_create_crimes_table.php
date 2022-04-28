ac59209e8d327b5f5b811a7debc984b331f1fdec
Timdev0 <quentin.peltier.pro@gmail.com>
2022-04-28 08:21:52 +0200

    feat(migrations): try migrations

---
+++ database/migrations/2022_04_27_153037_create_crimes_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Models\City;
+use App\Models\User;
+use App\Models\Weapon;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('crimes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class);
+            $table->foreignIdFor(Weapon::class);
+            $table->foreignIdFor(City::class);
+            $table->string('victim');
+            $table->date('crime_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('crimes');
+    }
+};
