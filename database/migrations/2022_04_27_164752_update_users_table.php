7ba6bac48f8763a59b55a526f82879543d45cd2a
Timdev0 <quentin.peltier.pro@gmail.com>
2022-04-28 08:21:52 +0200

    feat(migrations): try migrations

---
+++ database/migrations/2022_04_27_164752_update_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Crime;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::table('users', function (Blueprint $table) {
+        $table->foreignIdFor(Crime::class);
+    });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
