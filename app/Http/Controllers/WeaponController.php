8a865808be912d6860310dde1f1a19647ef836f7
Quentin Peltier <quentin.peltier.pro@gmail.com>
2022-04-29 13:12:50 +0200

    feat(weapons): add select for weapons type and unique type

--- app/Http/Controllers/WeaponController.php
+++ app/Http/Controllers/WeaponController.php
@@ -3,19 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Models\Weapon;
+use App\Models\Wtype;
 use Illuminate\Http\Request;
 
 class WeaponController extends Controller
 {
   public function addWeapon() {
-    //get all weapons from db
     $weapons = Weapon::all();
 
-    return view('add-weapon', compact(['weapons']));
+    $wtypes = Wtype::all();
+
+    return view('add-weapon', compact(['weapons', 'wtypes']));
   }
-//   public function addCrime(){
-//     $weapons = Weapon::all();
-//     $cities = City::all();
-//     return view('add-crime', compact(['cities', 'weapons']));
-// }
 }
